b0e912dd6099590e1a5ca8a91f9006186d9ab8c3
Nikk8744 <nikk8744@gmail.com>
2026-01-17 16:59:47 +0530

    fix: use proper url

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
     ],
   },
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+    const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000';
     return [
       {
         source: '/api/v1/:path*',
